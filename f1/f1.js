11aedd5ce9cc4ff2e457539557b6189598b3f974
KMen1 <oli70963@gmail.com>
2024-10-08 10:51:20 +0200

    feature: add functions

--- f1/f1.js
+++ f1/f1.js
@@ -1,7 +1,15 @@
+const APIAdress = "https://archive-api.open-meteo.com/v1/archive";
+async function f1(lat, long, timeStr) {
+  const url = new URL(APIAdress);
+  url.searchParams.append("latitude", lat);
+  url.searchParams.append("longitude", long);
+  url.searchParams.append("start_date", timeStr);
+  url.searchParams.append("end_date", timeStr);
+  url.searchParams.append("daily", "temperature_2m_mean");
 
-const APIAdress = 'https://archive-api.open-meteo.com/v1/archive?';
-async function f1 (lat, long, timeStr) {
-    //TODO
+  const response = await fetch(url);
+  const data = await response.json();
+  return Math.floor(data.daily.temperature_2m_mean[0]);
 }
 
 module.exports = f1;
--- f2/f2.js
+++ f2/f2.js
@@ -3,14 +3,19 @@ Készítsd el az előző feladat azon változatát ahol megadjuk az óraszámot 
 Csak az adott órára vonatkozó hőmérséklettel térjen vissza a függvény.
 */
 
-const locations = require('../locations.json');
-const APIAdress = 'https://archive-api.open-meteo.com/v1/archive?';
+const locations = require("../locations.json");
+const APIAdress = "https://archive-api.open-meteo.com/v1/archive?";
 
-const f2 = async (lat, long, timeStr, hour) => {
-    const str = APIAdress + `latitude=${lat}&longitude=${long}` +
-        `&start_date=${timeStr}&end_date=${timeStr}&hourly=temperature_2m`;
-    const rArr = await fetch(str)
-        .then(data => data.json());
-    return rArr.hourly.temperature_2m.at(hour);
+async function f2(lat, long, timeStr, hour) {
+  const url = new URL(APIAdress);
+  url.searchParams.append("latitude", lat);
+  url.searchParams.append("longitude", long);
+  url.searchParams.append("start_date", timeStr);
+  url.searchParams.append("end_date", timeStr);
+  url.searchParams.append("hourly", "temperature_2m");
+
+  const response = await fetch(url);
+  const data = await response.json();
+  return data.hourly.temperature_2m[hour];
 }
 module.exports = f2;
